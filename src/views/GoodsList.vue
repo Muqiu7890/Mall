2df1ebd572355ce9813215a517068e9846466976
Muqiu7890 <1337651840@qq.com>
2019-03-14 23:33:46 +0800

    modularization

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -1,16 +1,129 @@
 <template>
     <div>
-        商品列表{{$route.params.id}}
-        <!--命名路由-->
-        <router-link v-bind:to="{name: 'cart',params:{cartId: 123}}">Cart</router-link>
-        <router-view></router-view>
+      <nav-header></nav-header>
+      <nav-bread>Goods</nav-bread>
+        <div class="accessory-result-page accessory-page">
+            <div class="container">
+                <div class="filter-nav">
+                    <span class="sortby">Sort by:</span>
+                    <a href="javascript:void(0)" class="default cur">Default</a>
+                    <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+                    <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+                </div>
+                <div class="accessory-result">
+                    <!-- filter -->
+                    <div class="filter stopPop" id="filter">
+                        <dl class="filter-price">
+                            <dt>Price:</dt>
+                            <dd><a href="javascript:void(0)">All</a></dd>
+                            <dd>
+                                <a href="javascript:void(0)">0 - 100</a>
+                            </dd>
+                            <dd>
+                                <a href="javascript:void(0)">100 - 500</a>
+                            </dd>
+                            <dd>
+                                <a href="javascript:void(0)">500 - 1000</a>
+                            </dd>
+                            <dd>
+                                <a href="javascript:void(0)">1000 - 2000</a>
+                            </dd>
+                        </dl>
+                    </div>
+
+                    <!-- search result accessories list -->
+                    <div class="accessory-list-wrap">
+                        <div class="accessory-list col-4">
+                            <ul>
+                                <li>
+                                    <div class="pic">
+                                        <a href="#"><img src="static/1.jpg" alt=""></a>
+                                    </div>
+                                    <div class="main">
+                                        <div class="name">XX</div>
+                                        <div class="price">999</div>
+                                        <div class="btn-area">
+                                            <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="pic">
+                                        <a href="#"><img src="static/2.jpg" alt=""></a>
+                                    </div>
+                                    <div class="main">
+                                        <div class="name">XX</div>
+                                        <div class="price">1000</div>
+                                        <div class="btn-area">
+                                            <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="pic">
+                                        <a href="#"><img src="static/3.jpg" alt=""></a>
+                                    </div>
+                                    <div class="main">
+                                        <div class="name">XX</div>
+                                        <div class="price">500</div>
+                                        <div class="btn-area">
+                                            <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="pic">
+                                        <a href="#"><img src="static/4.jpg" alt=""></a>
+                                    </div>
+                                    <div class="main">
+                                        <div class="name">XX</div>
+                                        <div class="price">2499</div>
+                                        <div class="btn-area">
+                                            <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      <nav-footer></nav-footer>
     </div>
 </template>
 
 <script>
+    import '../assets/css/base.css'
+    import '../assets/css/product.css'
+    import NavHeader from '../components/NavHeader.vue'
+    import NavFooter from '../components/NavFooter.vue'
+    import NavBread from '../components/NavBread.vue'
+    import axios from 'axios'
     export default {
-        name: "GoodsList"
-
+      name: "GoodsList",
+      data() {
+        return {
+          goodList: []
+        }
+      },
+      components: {
+        NavHeader,
+        NavFooter,
+        NavBread
+      },
+      mounted: function () {
+        this.getGoodsList()
+      },
+      methods: {
+        getGoodsList() {
+          axios.get('../../static/data/goods.json').then((res) => {
+            alert(res.data.result)
+          }).catch((res) => {
+            window.console.log(res)
+          })
+        }
+      }
     }
 </script>
 
